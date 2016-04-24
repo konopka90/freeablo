02d093d657696770d3fd7b4264f1ab16ec96c2c8
xxx <konopka90@gmail.com>
2016-04-24 03:16:14 +0200

    Naming convention + next improvements

--- resources/python/tradeMenu.py
+++ resources/python/tradeMenu.py
@@ -15,14 +15,10 @@ class TradeMenu(object):
             Needed to access members from rml callbacks, eg: <span id="..." onclick="selfName.setSelected(...
         containerId -- The id of the element within which to place the menu rml
         entries -- A list of dicts, each one containing the following fields:
-            text -- Mandatory. The text for this entry
+            item -- Mandatory. Item name
+            price -- Mandatory. Item price
             func -- Optional. The function to be called when a user presses return with this entry hilighted
-            textFunc -- Optional. The function to be called (without () at the end) when a user clicks on 
-                the entry. Must be accessible from within the scope of rml callbacks. 
-                Required for similar reasons to the selfName field above
-        fmtSelected -- The format for rml for entries when they are selected. Must contain %s somewhere, which
-            will be replaced with entry.text for each entry.
-        fmtNotSelected -- Same as above, but for unselected entries.
+
         onSelect -- Callback function when user selects menu entry
         """
 
@@ -54,7 +50,7 @@ class TradeMenu(object):
             onclick = (val["strFunc"]+"({0})").format(args) if "strFunc" in val else ""
             entryStr = '<span class="trade-menu-entry" id="tradeMenuEntry%05d" onmouseover="%s.mouseover(%05d)" onclick="%s.activate()" style="display:block;">' % (id, self.selfName, id, self.selfName)
             entryStr +=     '<span class="empty-pentagon-left" id="pentagon-left-%05d"></span>' % id
-            entryStr +=     '<span class="trade-menu-item-name">%s</span><span class="trade-menu-item-price">%d</span>' % (val["text"], val["price"])
+            entryStr +=     '<span class="trade-menu-item-name">%s</span><span class="trade-menu-item-price">%d</span>' % (val["item"], val["price"])
             entryStr +=     '<span class="empty-pentagon-right" id="pentagon-right-%05d"></span>' % id
             entryStr +=     '<span class="trade-menu-item-description">'
             entryStr +=         'armor: 2 dur: 15/15, no required attributes'
@@ -67,9 +63,7 @@ class TradeMenu(object):
         container.inner_rml = menuHtmlStr
 
     def mouseover(self, num):
-        print "mouse over %d" % num
         self.setSelected(num)
-
 
     def deleteEntry(self, num):
         if num < len(self.entries):
@@ -77,6 +71,9 @@ class TradeMenu(object):
             self.initMenu()
             if self.onSelect != None:
                 self.onSelect(self.doc, 0)
+
+            if num >= len(self.entries):
+                num = len(self.entries) - 1
 
             self.onDeleteEntrySetSelected(num)
 
